9d0121b04b723ea1ac03792840c66f00e67ab9b2
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2025-01-10 11:31:34 -0500

    Add local routing, old edits

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,35 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!!" />
+      <!-- <HelloWorld msg="You did it!!" /> -->
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
+        <RouterLink to="/contact-me">Contact</RouterLink>
+        <RouterLink to="/site-map">Site Map</RouterLink>
       </nav>
+
+      <div class="header-buttons">
+        <a href="#">
+        </a>
+        <a href="https://github.com/Stanley-C-02" target="_blank">
+          <img alt="My GitHub Profile" src="@/assets/github-mark/github-mark.svg" height="125">
+        </a>
+        <a href="https://www.linkedin.com/in/stanleyc02/" target="_blank">
+          <img alt="My LinkedIn Profile" src="@/assets/linkedin-logos/LI-In-Bug.png" height="125">
+        </a>
+        <!-- <a href="https://github.com/Stanley-C-02">
+          <img alt="My Placeholder Profile" src="@/assets/github-mark/github-mark.svg" width="125" height="125">
+        </a>
+        <a href="https://github.com/Stanley-C-02">
+          <img alt="My Placeholder Profile" src="@/assets/github-mark/github-mark.svg" width="125" height="125">
+        </a> -->
+        <button type="button" link>
+          <img alt="Page Settings" src="" height="125" />
+        </button>
+      </div>
     </div>
   </header>
 
