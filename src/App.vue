30a3ef32c86df16f88e8bee90e9d2d9caefe6a8f
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2025-06-28 15:42:09 -0400

    Start adding site content

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,37 @@
 <script setup lang="ts">
+import logo from "@/assets/logo.svg"
+import { mdiGithub, mdiLinkedin } from "@mdi/js";
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <VApp style="min-width: 1024px;">
+    <VAppBar>
+      <template #prepend>
+        <nav class="d-flex align-center ga-1">
+          <VImg alt="Vue logo" class="logo" :src="logo" width="64" />
+          <VBtn to="/">Home</VBtn>
+          <VBtn to="/about">About</VBtn>
+        </nav>
+      </template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!!" />
+      <template #append>
+        <nav class="d-flex align-center ga-2">
+          <VBtn target="_blank" :icon="mdiGithub" href="https://github.com/Stanley-C-02" title="GitHub (opens in new tab)"></VBtn>
+          <VBtn target="_blank" :icon="mdiLinkedin" href="https://www.linkedin.com/in/stanleyc02/" title="LinkedIn (opens in new tab)"></VBtn>
+        </nav>
+      </template>
+    </VAppBar>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <VMain>
+      <RouterView />
+    </VMain>
 
-  <RouterView />
+    <VFooter class="px-12 py-4">
+      <h2>Footer</h2>
+    </VFooter>
+  </VApp>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,70 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import { mdiGithub, mdiReact, mdiSchool } from '@mdi/js'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- Hero -->
+  <section class="px-12 bg-grey-lighten-1 d-flex flex-column justify-center" style="height: 50vh;">
+    <h1 class="text-h2 font-weight-medium">Hello there, and welcome!</h1>
+    <p class="text-h4 font-weight-light">I'm Stanley, a software engineer excited to create and work with practical digital solutions.</p>
+    <div class="mt-4">
+      <VChip class="ga-1" color="blue">
+        <template #prepend>
+          <VIcon :icon="mdiSchool" />
+        </template>
+        <span class="font-weight-medium">TMU</span>
+      </VChip>
+    </div>
+  </section>
+
+  <!-- Timeline -->
+  <section class="px-12 py-16">
+    <h2 class="text-h3 font-weight-medium">Some of my work</h2>
+    <VTimeline>
+      <VTimelineItem class="bg-cyan-darken-4">
+        <template #opposite>
+          <div class="d-flex flex-column">
+            <div class="d-flex">
+              <h3 class="text-h4">Project Name</h3>
+              <VBtn :icon="mdiGithub" title="Project GitHub (opens in new tab)" />
+            </div>
+            <div>
+              <VChip class="ga-1" color="blue">
+                <template #prepend>
+                  <VIcon :icon="mdiReact" />
+                </template>
+                <span class="font-weight-medium">React</span>
+              </VChip>
+            </div>
+            <p class="text-body-1">Project Description. Est animi perferendis eius aut labore. Necessitatibus aut maiores animi non aut sit. Deleniti doloremque architecto. Est expedita perspiciatis facilis nihil sit vitae.</p>
+          </div>
+        </template>
+        <VImg alt="Project 1" src="" width="320" height="256" />
+      </VTimelineItem>
+      <VTimelineItem class="bg-cyan-darken-2">
+        <template #opposite>
+          <div class="d-flex flex-column">
+            <div class="d-flex">
+              <h3 class="text-h4">Project Name</h3>
+              <VBtn :icon="mdiGithub" title="Project GitHub (opens in new tab)" />
+            </div>
+            <div>
+              <VChip class="ga-1" color="blue">
+                <template #prepend>
+                  <VIcon :icon="mdiReact" />
+                </template>
+                <span class="font-weight-medium">React</span>
+              </VChip>
+            </div>
+            <p class="text-body-1">Project Description. Enim alias quia ipsam temporibus officia ea reiciendis consequatur voluptatem. Fugit dolore id impedit dolor qui in non. Accusamus nisi ipsam quo a laudantium autem. Qui voluptatem voluptatem quae temporibus aliquid quia quae eum et.</p>
+          </div>
+        </template>
+        <VImg alt="Project 1" src="" width="320" height="256" />
+      </VTimelineItem>
+    </VTimeline>
+  </section>
 </template>
+
+<style scoped>
+</style>
