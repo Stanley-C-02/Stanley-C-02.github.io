c3acccd0fd4daff13bbd619d5c4e255d2da44fd7
Stanley Chow <51177611+ForTheWindFTW@users.noreply.github.com>
2025-10-15 13:50:09 -0400

    Small refactoring to re-deploy

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
 import MyFooter from "./components/MyFooter.vue";
-import { VBtn } from "vuetify/components";
 </script>
 
 <template>
   <VApp style="min-width: 1280px; min-height: 100vh;">
-  <!-- <VApp class="mx-auto"> -->
     <Navbar />
 
     <VMain id="main">
